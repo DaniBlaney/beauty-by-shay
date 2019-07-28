63d7b3d4b08cf0c2f818815ecf8bc0417b1e6d76
DaniBlaney <daniblaney@gmail.com>
2019-07-27 21:48:51 -0400

    const images in index js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -16,13 +16,13 @@ const insideStyles = {
   transform: "translate(-50%,-50%)"
 };
 const image1 =
-  "https://images.unsplash.com/photo-1498092651296-641e88c3b057?auto=format&fit=crop&w=1778&q=60&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D";
+  "images/mirror.jpg";
 const image2 =
-  "https://img00.deviantart.net/2bd0/i/2009/276/c/9/magic_forrest_wallpaper_by_goergen.jpg";
+  "images/hair.png";
 const image3 =
-  "https://brightcove04pmdo-a.akamaihd.net/5104226627001/5104226627001_5297440765001_5280261645001-vs.jpg?pubId=5104226627001&videoId=5280261645001";
+  "images/chris.png";
 const image4 =
-  "https://images.fineartamerica.com/images/artworkimages/mediumlarge/1/empire-state-building-black-and-white-square-format-john-farnan.jpg";
+  "images/girl.jpg";
 
 const App = () => (
   <div style={styles}>
