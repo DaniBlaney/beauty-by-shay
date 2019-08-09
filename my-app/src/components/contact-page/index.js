648a19ee5e9aaea42e0c95e84e966c5e4af06522
DaniBlaney <daniblaney@gmail.com>
2019-08-09 16:55:13 -0400

    added jumbotron to contact page

--- my-app/src/components/contact-page/index.js
+++ my-app/src/components/contact-page/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Row, Col, Card, Body, Title, Text, Nav, Navbar, Dropdown, ButtonGroup, NavItem, Link, Brand, Button, FormControl, Form, Image, Container } from 'react-bootstrap';
+import { Row, Col, Card, Body, Title, Text, Nav, Navbar, Dropdown, ButtonGroup, NavItem, Link, Brand, Button, FormControl, Form, Image, Container, Jumbotron } from 'react-bootstrap';
 import NavBarComponent from "../navbar";
 import FooterComponent from '../footer';
 
@@ -8,10 +8,19 @@ import FooterComponent from '../footer';
 const Contact = () => {
   return (
     <>
-    <NavBarComponent/>
-
-    <FooterComponent/>
+      <NavBarComponent />
+      <Jumbotron align="center">
+        <h1>Coming Soon!</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for calling
+          extra attention to featured content or information.
+        </p>
+        <p>
+          <Button href="/" variant="primary">Return Home</Button>
+        </p>
+      </Jumbotron>
+      <FooterComponent />
     </>
-);
+  );
 }
 export default Contact;
