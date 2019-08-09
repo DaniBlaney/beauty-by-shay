288224f31bb93bf04fdf66aa56dc56a4ef3c4092
DaniBlaney <daniblaney@gmail.com>
2019-08-09 12:29:29 -0400

    added gallery button routes to gallery page

--- my-app/src/components/Home/index.js
+++ my-app/src/components/Home/index.js
@@ -5,6 +5,7 @@ import NavBarComponent from "../navbar";
 import FooterComponent from '../footer';
 
 
+
 const HomePage = () => {
   return (
     <>
@@ -13,7 +14,7 @@ const HomePage = () => {
       <Container fluid>
         <Row>
           <Col>
-            <Card style={{ width: '20rem' }}>
+            <Card className="cardStyle" style={{ width: '20rem' }}>
               <Card.Img variant="top" src="./images/haircut.png" />
               <Card.Body>
                 <Card.Title>About me</Card.Title>
@@ -24,57 +25,60 @@ const HomePage = () => {
             </Card>
           </Col>
           <Col>
-            <Card  style={{ width: '20rem' }}>
+            <Card className="cardStyle" style={{ width: '20rem' }}>
               <Card.Img variant="top" src="./images/hairheart.png" />
               <Card.Body>
                 <Card.Title>Why I love my profession</Card.Title>
                 <Card.Text>
                   I find it rewarding to help people bring out their own natural beauty.
-            </Card.Text>
+                </Card.Text>
               </Card.Body>
             </Card>
           </Col>
           <Col>
-            <Card style={{ width: '20rem' }}>
+            <Card className="cardStyle" style={{ width: '20rem' }}>
               <Card.Img variant="top" src="./images/shayy.png" />
               <Card.Body>
+              <Button href="/gallery" variant="light" block>
                 <Card.Title>Gallery</Card.Title>
+              </Button>
                 <Card.Text>
                   Check out some of the work I have done!
-            </Card.Text>
+
+                </Card.Text>
               </Card.Body>
             </Card>
           </Col>
         </Row>
       </Container>
-      <hr/>
+      <hr />
       <Container fluid>
         <Row>
           <Col md={6}>
-          <Card.Img src="./images/fringe.png" />
+            <Card.Img className="cardStyle" style={{ width: '33em' }} src="./images/fringe.png" />
           </Col>
           <Col md={6}>
-          <Card >
-            <Card.Header>Certifications</Card.Header>
-            <Card.Body>
-              <blockquote className="blockquote mb-0">
-                <ul>
-                  {' '}
-                  <li>{"\u2728"} La Bella Hair Extension Certified |</li>
-                  <li>{"\u2728"} Certified in Cut it Out |</li>
-                  <li>{"\u2728"} Wella Custom Product Certified |</li>
-                  <li>{"\u2728"} Wella Custom Color Essentials Cert |</li>
-                  <li>{"\u2728"} Barbicide Certified |</li>
-                  <li>{"\u2728"} Eyes On Cancer Melanoma Awareness Certified |</li>
-                  <li>{"\u2728"} CPR & First Aid Certified |</li>
-                  {' '}
-                </ul>
-                <footer className="blockquote-footer">
-                Additional skills: <cite title="Source Title">Sign Language</cite>
-                </footer>
-              </blockquote>
-            </Card.Body>
-          </Card>
+            <Card className="cardStyle">
+              <Card.Header>Certifications</Card.Header>
+              <Card.Body>
+                <blockquote className="blockquote mb-0">
+                  <ul>
+                    {' '}
+                    <li>{"\u2728"} La Bella Hair Extension Certified |</li>
+                    <li>{"\u2728"} Certified in Cut it Out |</li>
+                    <li>{"\u2728"} Wella Custom Product Certified |</li>
+                    <li>{"\u2728"} Wella Custom Color Essentials Cert |</li>
+                    <li>{"\u2728"} Barbicide Certified |</li>
+                    <li>{"\u2728"} Eyes On Cancer Melanoma Awareness Certified |</li>
+                    <li>{"\u2728"} CPR & First Aid Certified |</li>
+                    {' '}
+                  </ul>
+                  <footer className="blockquote-footer">
+                    Additional skills: <cite title="Source Title">Sign Language</cite>
+                  </footer>
+                </blockquote>
+              </Card.Body>
+            </Card>
           </Col>
         </Row>
       </Container>
--- my-app/src/components/footer.js
+++ my-app/src/components/footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Col, Row, Navbar, Nav, Link, Item } from 'react-bootstrap';
+import { Container, Col, Row, Navbar, Nav, Link, Item, Button } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import 'font-awesome/css/font-awesome.min.css'
 
@@ -12,10 +12,10 @@ const FooterComponent = () => {
             <a>&copy; BeautyByShay 2019 {"\u2728"}</a>
           </Nav.Item>
           <Nav.Link>
-            <a href="https://www.instagram.com/beautyy_by_shay/?hl=en" target="_blank" className="btn btn-social-icon btn-instagram">
+            <Button variant="light" href="https://www.instagram.com/beautyy_by_shay/?hl=en" target="_blank" className="btn btn-social-icon btn-instagram">
               Follow me on Instagram @Beautyy_by_shay <FontAwesomeIcon icon="instagram" />
               <span className="fa fa-instagram"></span>
-            </a>
+            </Button>
             <a href="#" className="btn btn-social-icon btn-facebook">
               <span className="fa fa-facebook"></span>
             </a>
