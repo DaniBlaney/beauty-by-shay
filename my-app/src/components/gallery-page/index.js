c5e7ea8b8a17f81930cef92e996c5841fd6d003c
DaniBlaney <daniblaney@gmail.com>
2019-08-09 15:35:52 -0400

    started adding photos to gallery

--- my-app/src/components/gallery-page/index.js
+++ my-app/src/components/gallery-page/index.js
@@ -8,10 +8,68 @@ import FooterComponent from '../footer';
 const Gallery = () => {
   return (
     <>
-    <NavBarComponent/>
+      <NavBarComponent />
+      <Container>
+        <Row>
+          <Col xs={6} md={3}>
+            <Image className="imageSize" src="./images/color1.jpg" rounded />
+          </Col>
+          <Col xs={6} md={3}>
+            <Image className="imageSize" src="./images/color2.jpg" rounded />
+          </Col>
+          <Col xs={6} md={3}>
+            <Image className="imageSize" src="./images/color3.jpg" rounded />
+          </Col>
+          <Col xs={6} md={3}>
+            <Image className="imageSize" src="./images/color4.jpg" rounded />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={6} md={3}>
+            <Image className="imageSize" src="./images/roots1.jpg" rounded />
+          </Col>
+          <Col xs={6} md={3}>
+            <Image className="imageSize" src="./images/roots2.jpg" rounded />
+          </Col>
+          <Col xs={6} md={3}>
+            <Image className="imageSize" src="./images/roots3.jpg" rounded />
+          </Col>
+          <Col xs={6} md={3}>
+            <Image className="imageSize" src="./images/roots4.jpg" rounded />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={6} md={3}>
+            <Image className="imageSize" src="./images/correction1.jpg" rounded />
+          </Col>
+          <Col xs={6} md={3}>
+            <Image className="imageSize" src="./images/correction2.jpg" rounded />
+          </Col>
+          <Col xs={6} md={3}>
+            <Image className="imageSize" src="./images/correction4.jpg" rounded />
+          </Col>
+          <Col xs={6} md={3}>
+            <Image className="imageSize" src="./images/correction3.jpg" rounded />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={6} md={3}>
+            <Image className="imageSize" src="./images/color1.jpg" rounded />
+          </Col>
+          <Col xs={6} md={3}>
+            <Image className="imageSize" src="./images/color2.jpg" rounded />
+          </Col>
+          <Col xs={6} md={3}>
+            <Image className="imageSize" src="./images/color3.jpg" rounded />
+          </Col>
+          <Col xs={6} md={3}>
+            <Image className="imageSize" src="./images/color4.jpg" rounded />
+          </Col>
+        </Row>
 
-    <FooterComponent/>
+      </Container>
+      <FooterComponent />
     </>
-);
+  );
 }
 export default Gallery;
